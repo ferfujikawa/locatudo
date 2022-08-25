2d6aa3511c57db53b8262a87d30d8bbba4f593b4
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-25 12:42:09 -0300

    Criação da entidade Pessoa.

---
+++ Locatudo.Dominio/Entidades/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using Locatudo.Compartilhado.Entidades;
+
+namespace Locatudo.Dominio.Entidades
+{
+    public class Pessoa : EntidadeAbstrata
+    {
+        public Pessoa(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+    }
+}
