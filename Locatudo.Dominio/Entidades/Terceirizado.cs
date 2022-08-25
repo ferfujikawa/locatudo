bf1ca48a294520f0c72f2856c90588d0134ddf6e
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-25 12:57:00 -0300

    Criação da entidade Terceirizado e objeto de valor NomePessoaJuridica.

---
+++ Locatudo.Dominio/Entidades/Terceirizado.cs
@@ -0,0 +1,14 @@
+﻿using Locatudo.Dominio.ObjetosDeValor;
+
+namespace Locatudo.Dominio.Entidades
+{
+    public class Terceirizado : Pessoa
+    {
+        public Terceirizado(NomePessoaFisica nome, Email email, NomePessoaJuridica empresa) : base(nome, email)
+        {
+            Empresa = empresa;
+        }
+
+        public NomePessoaJuridica Empresa { get; private set; }
+    }
+}
---
+++ Locatudo.Dominio/ObjetosDeValor/NomePessoaJuridica.cs
@@ -0,0 +1,12 @@
+﻿namespace Locatudo.Dominio.ObjetosDeValor
+{
+    public class NomePessoaJuridica
+    {
+        public NomePessoaJuridica(string razaoSocial)
+        {
+            RazaoSocial = razaoSocial;
+        }
+
+        public string RazaoSocial { get; private set; }
+    }
+}
