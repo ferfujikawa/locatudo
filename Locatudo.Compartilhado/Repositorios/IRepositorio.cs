e54c465c664c0d46b188193b85062f01792cc1fc
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-26 12:36:21 -0300

    Criação do método Alterar em IRepositorio.

--- Locatudo.Compartilhado/Repositorios/IRepositorio.cs
+++ Locatudo.Compartilhado/Repositorios/IRepositorio.cs
@@ -5,8 +5,9 @@ namespace Locatudo.Compartilhado.Repositorios
     public interface IRepositorio<T> where T : EntidadeAbstrata
     {
         void Criar(T entidade);
-        void Excluir(Guid id);
-        T? GetById(Guid id);
         IEnumerable<T> Listar();
+        T? ObterPorId(Guid id);
+        void Alterar(T entidade);
+        void Excluir(Guid id);
     }
 }
