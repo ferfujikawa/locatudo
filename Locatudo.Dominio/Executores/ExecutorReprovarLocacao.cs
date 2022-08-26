ec58f7bd1c17b145544454380a27c3155a91b32f
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-26 20:35:18 -0300

    Criação de executor e comando para reprovar Locacao.

---
+++ Locatudo.Dominio/Executores/ExecutorReprovarLocacao.cs
@@ -0,0 +1,34 @@
+﻿using Locatudo.Compartilhado.Executores;
+using Locatudo.Dominio.Executores.Comandos;
+using Locatudo.Dominio.Repositorios;
+
+namespace Locatudo.Dominio.Executores
+{
+    public class ExecutorReprovarLocacao : IExecutor<ComandoReprovarLocacao>
+    {
+        private readonly IRepositorioLocacao _repositorioLocacao;
+        private readonly IRepositorioFuncionario _repositorioFuncionario;
+
+        public ExecutorReprovarLocacao(
+            IRepositorioLocacao repositorioLocacao,
+            IRepositorioFuncionario repositorioFuncionario)
+        {
+            _repositorioLocacao = repositorioLocacao;
+            _repositorioFuncionario = repositorioFuncionario;
+        }
+
+        public void Executar(ComandoReprovarLocacao comando)
+        {
+            var aprovador = _repositorioFuncionario.ObterPorId(comando.IdAprovador);
+            if (aprovador == null)
+                throw new Exception("Funcionário não encontrado.");
+
+            var locacao = _repositorioLocacao.ObterPorId(comando.IdLocacao);
+            if (locacao == null)
+                throw new Exception("Locacação não encontrada.");
+
+            if (locacao.Reprovar(aprovador) == false)
+                throw new Exception("A situação atual da locação não permite reprovação.");
+        }
+    }
+}
