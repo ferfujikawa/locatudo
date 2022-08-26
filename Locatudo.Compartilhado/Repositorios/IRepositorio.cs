93a889ec93a54c7f89dc45390d9eaf4bc6d894b8
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-26 12:01:18 -0300

    Criação da interface IRepositorio.

---
+++ Locatudo.Compartilhado/Repositorios/IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using Locatudo.Compartilhado.Entidades;
+
+namespace Locatudo.Compartilhado.Repositorios
+{
+    public interface IRepositorio<T> where T : EntidadeAbstrata
+    {
+        void Criar(T entidade);
+        void Excluir(Guid id);
+        T? GetById(Guid id);
+        IEnumerable<T> Listar();
+    }
+}
